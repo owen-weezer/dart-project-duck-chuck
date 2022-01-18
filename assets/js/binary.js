2ec4e915d625575bcf089930a7d40c7a64a5f4f7
owen-weezer <95237851+owen-weezer@users.noreply.github.com>
2022-01-18 10:45:44 -0500

    1.0.0

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.7.24/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.1","target":"arcade","targetVersion":"1.7.24","repo":"owen-weezer/dart-project-duck-chuck"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.7.24/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"1.0.0","target":"arcade","targetVersion":"1.7.24","repo":"owen-weezer/dart-project-duck-chuck"}
 // total=1663808 new=7.58% cached=87.29% other=5.13%
 (function (ectx) {
 'use strict';
@@ -506,7 +506,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P21869(s) {
+function _main___P21882(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -1028,13 +1028,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P21869.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P21869.continuations = [  ]
+_main___P21882.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P21882.continuations = [  ]
 
-function _main___P21869_mk(s) {
+function _main___P21882_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P21869, depth: s.depth + 1,
+        parent: s, fn: _main___P21882, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -55865,5 +55865,5 @@ const animation_PathNode__C2955_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, ["transparency16___3904","tile2___3906","tile1___3905","obstacle___3889","bystander___3890","myDart___3900","DART_X_VALUE___3902","DART_Y_VALUE___3901","redsnake___3896","greensnake___3897","obstacle___3899","bad_guy___3898"])
 
-return _main___P21869
+return _main___P21882
 })
